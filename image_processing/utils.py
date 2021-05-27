a144badb0a16554e6fbadccc27966a13d6ce84bb
Omkar <omkar@cyware.com>
2021-05-27 23:43:10 +0530

    util(Image, Video): Rescaling Utility for Image and Video Frames

--- image_processing/utils.py
+++ image_processing/utils.py
@@ -3,8 +3,17 @@ import cv2 as cv
 
 class ImageUtil:
     @staticmethod
-    def read_image(path):
+    def read_image(path, scale=1):
         img = cv.imread(path)
-        cv.imshow('CV Image Header', img)
+        scaled_img = ImageUtil.rescaleFrame(frame=img, scale=scale)
+        cv.imshow('CV Image Header', scaled_img)
         cv.waitKey(0)
 
+    @staticmethod
+    def rescaleFrame(frame, scale=1):
+        height = int(frame.shape[0] * scale)
+        width = int(frame.shape[1] * scale)
+
+        dimensions = (width, height)
+
+        return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
--- video_processing/utils.py
+++ video_processing/utils.py
@@ -3,14 +3,24 @@ import cv2 as cv
 
 class VideoUtil:
     @staticmethod
-    def read_video(path):
+    def read_video(path, scale=1):
         capture = cv.VideoCapture(path)
         while True:
             flag, frame = capture.read()
-            cv.imshow('CV Video Header', frame)
+            scaled_frame = VideoUtil.rescaleFrame(frame=frame, scale=scale)
+            cv.imshow('CV Video Header', scaled_frame)
 
             if cv.waitKey(20) & 0xFF == ord('d'):
                 break
         
         capture.release()
         cv.destroyAllWindows()
+
+    @staticmethod
+    def rescaleFrame(frame, scale=1):
+        height = int(frame.shape[0] * scale)
+        width = int(frame.shape[1] * scale)
+
+        dimensions = (width, height)
+
+        return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
